c5d44457ffa4055f545f11de5f6a70e251480bc3
Niklas <niklas.boeing@gmx.de>
2024-07-01 09:36:23 +0200

    Deleted old code fragments

--- src/cmc.cxx
+++ src/cmc.cxx
@@ -12,6 +12,8 @@ CmcInitialize()
 {
   MPIInitialize();
 
+  //TODO: Initialize t8code
+
   #ifdef CMC_ENABLE_MPI
   int err, rank;
   /* Get the rank of the process */
@@ -38,6 +40,8 @@ CmcFinalize()
   MPICheckError(err);
   #endif
 
+  //TODO: Finalize t8code
+
   MPIFinalize();
 
   #ifdef CMC_ENABLE_MPI
@@ -52,102 +56,3 @@ CmcFinalize()
 }
 
 }
-
-#if 0
-/** Initialize cmc and it's sub-modules */
-void
-cmc_initialize()
-{
-  /* Initialize MPI */
-  cmc_mpi_initialize();
-  /* Initialize t8code */
-  //TODO:
-  //cmc_t8code_initialize(MPI_COMM_WORLD);
-  #ifdef CMC_ENABLE_MPI
-  int err, rank;
-  /* Get the rank of the process */
-  err = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  cmc_mpi_check_err(err);
-  /* Only print out the initilization info on rank zero */
-  if (rank == 0)
-  {
-    std::cout << "[cmc] cmc has been initialized." << std::endl; 
-  }
-  #else
-  std::cout << "[cmc] cmc has been initialized." << std::endl; 
-  #endif
-}
-
-/** Initialize cmc and it's sub-modules with a specific MPI Communicator */
-void
-cmc_initialize_mpi_comm(MPI_Comm comm)
-{
-  /* Initialize MPI */
-  cmc_mpi_initialize();
-  /* Initialize t8code */
-  //TODO:
-  //ccmc_t8code_initialize(comm);
-  #ifdef CMC_ENABLE_MPI
-  int err, rank;
-  /* Get the rank of the process */
-  err = MPI_Comm_rank(comm, &rank);
-  cmc_mpi_check_err(err);
-  /* Only print out the initilization info on rank zero */
-  if (rank == 0)
-  {
-    std::cout << "[cmc] cmc has been initialized." << std::endl;
-  }
-  #else
-  std::cout << "[cmc] cmc has been initialized." << std::endl;
-  #endif
-}
-
-/** Finalize cmc and it's submodules */
-void
-cmc_finalize()
-{
-  #ifdef CMC_ENABLE_MPI
-  int err, rank;
-  /* Get the rank of the process */
-  err = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  cmc_mpi_check_err(err);
-  #endif
-
-  /* Finalize t8code */
-  //TODO:
-  //ccmc_t8code_finalize(0);
-  /* Finalize MPI */
-  cmc_mpi_finalize();
-  #ifdef CMC_ENABLE_MPI
-  /* Only print out the initilization info on rank zero */
-  if (rank == 0)
-  {
-    std::cout << "[cmc] cmc has been finalized." << std::endl;
-  }
-  #else
-  /* Print out the finalizing info */
-  std::cout << "[cmc] cmc has been finalized." << std::endl;
-  #endif
-}
-
-/** Finalize cmc and it's submodules except MPI */
-void
-cmc_finalize_without_mpi()
-{
-  /* Finalize t8code */
-  //TODO:
-  //ccmc_t8code_finalize(0);
-  #ifdef CMC_ENABLE_MPI
-  int err, rank; 
-  err = MPI_Comm_rank(MPI_COMM_WORLD, &rank);
-  cmc_mpi_check_err(err);
-  /* Only print out the finalizing info on rank zero of the MPI_WORLD_COMM */
-  if (rank == 0)
-  {
-    std::cout << "[cmc] cmc has been finalized." << std::endl;
-  }
-  #else
-  std::cout << "[cmc] cmc has been finalized." << std::endl;
-  #endif
-}
-#endif
--- src/cmc.hxx
+++ src/cmc.hxx
@@ -1,5 +1,5 @@
-#ifndef CMC_H
-#define CMC_H
+#ifndef CMC_HXX
+#define CMC_HXX
 
 /* Include the configuration macros describing the linkage status to other libraries */
 #include "cmc_config.h"
@@ -13,52 +13,5 @@ void CmcInitialize();
 void CmcFinalize();
 
 }
-#if 0
 
-#ifdef __cplusplus
-#ifdef CMC_ENABLE_FORTRAN
-/* In case the Fortran interface is built, these function needs to be callable from C-wrapper scripts.
- * In order to prevent name-mangling, we have to declare them as extern "C"
- */
-extern "C" {
-#endif
-#endif
-
-/**
- * @brief Initialize cmc by initializing all sub-modules (e.g. MPI, t8code, etc..)
- * @note If MPI is enabled, the MPI Communicator to use, will be MPI_COMM_WORLD
- */
-void
-cmc_initialize();
-
-/**
- * @brief Initialize cmc by initializing all sub-modules (e.g. MPI, t8code, etc..) with a specific MPI Communicator
- * 
- * @param comm The communicator to be used by cmc, t8code, etc.
- */
-void
-cmc_initialize_mpi_comm(MPI_Comm comm);
-
-/**
- * @brief Finalize cmc by finalizing all sub-modules (e.g. MPI, t8code, etc..)
- */
-void
-cmc_finalize();
-
-/**
- * @brief Finalize cmc by finalizing all sub-modules (e.g. t8code, etc..) except for MPI.
- */
-void
-cmc_finalize_without_mpi();
-
-#ifdef __cplusplus
-#ifdef CMC_ENABLE_FORTRAN
-}
-#endif
-#endif
-
-
-#endif
-
-
-#endif /* CMC_H */
+#endif /* CMC_HXX */
