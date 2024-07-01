d96d8724e240578179bcf2666784c0946f513128
Niklas <niklas.boeing@gmx.de>
2024-07-01 16:32:40 +0200

    Added files for a general compression functions

---
+++ src/cmc_compression.hxx
@@ -0,0 +1,17 @@
+#ifndef CMC_COMPRESSION_HXX
+#define CMC_COMPRESSION_HXX
+
+#include "cmc.hxx"
+#include "utilities/cmc_input_variable.hxx"
+#include "utilities/cmc_output_variable.hxx"
+#include "lossy/cmc_amr_lossy_compression_settings.hxx"
+
+#include <vector>
+
+namespace cmc
+{
+
+
+}
+
+#endif /* !CMC_COMPRESSION_HXX */
