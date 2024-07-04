fb56e913a823deb7ab0369903fb96f4809db6f42
Niklas <niklas.boeing@gmx.de>
2024-07-04 14:37:34 +0200

    Started the implementation of a general decompression function from a file

--- src/cmc_compression.cxx
+++ src/cmc_compression.cxx
@@ -1,8 +1,27 @@
 #include "cmc_compression.hxx"
 #include "netcdf/cmc_nc_reader.hxx"
+#include "utilities/cmc_log_functions.hxx"
+
+#include <vector>
 
 namespace cmc
 {
+
+
+static
+std::vector<OutputVar>
+GeneralDecompressionByAdaptiveCoarsening()
+{
+    return std::vector<OutputVar>();
+}
+
+static
+std::vector<OutputVar>
+GeneralDecompressionByPrefixExtraction()
+{
+    return std::vector<OutputVar>();
+}
+
 
 std::vector<OutputVar>
 Decompress(const std::string& file_name)
@@ -14,9 +33,40 @@ Decompress(const std::string& file_name)
     std::vector<NcAttribute> global_attributes = reader.ReadGlobalAttrtibutes();
 
     /* Check for a given attribute indicating the compression which has been used */
-    //auto comp_scheme_iter = FindAttribute(global_attributes, "compression_scheme");
+    auto comp_scheme_iter = FindAttribute(global_attributes, kCompressionSchemeAttrName);
 
+    /* Check if the attribute has been found */
+    if (comp_scheme_iter == global_attributes.end())
+    {
+        cmc_global_msg("There is no 'compression_scheme' attribute attached to this file. Therefore, no decompression is applicable.");
+        return std::vector<OutputVar>();
+    } else 
+    {
+        /* Get the attribute and check it's value */
+        const NcAttribute& comp_scheme_attr = *comp_scheme_iter;
 
+        /* Get the value of the compression scheme */
+        const CompressionScheme scheme = static_cast<CompressionScheme>(std::get<CompressionSchemeType>(comp_scheme_attr.GetValue()));
+
+        switch (scheme)
+        {
+            case CompressionScheme::AdaptiveCoarsening:
+                return GeneralDecompressionByAdaptiveCoarsening(); 
+            break;
+            case CompressionScheme::PrefixExtraction:
+                return GeneralDecompressionByAdaptiveCoarsening();
+            break;
+            case CompressionScheme::AdaptiveCoarseningPlusPrefixExtraction:
+                return GeneralDecompressionByAdaptiveCoarsening();
+            break;
+            case CompressionScheme::NoCompression:
+                cmc_global_msg("No compression has been applied. Therefore, no decompression is applicable.");
+                return std::vector<OutputVar>();
+            break;
+            default:
+                return std::vector<OutputVar>();
+        }
+    }
 }
 
 }
--- src/netcdf/cmc_nc_io_naming_conventions.hxx
+++ src/netcdf/cmc_nc_io_naming_conventions.hxx
@@ -1,9 +1,16 @@
-#ifndef CMC_NC_IO_NAMING_CONVENTIONS
-#define CMC_NC_IO_NAMING_CONVENTIONS
+#ifndef CMC_NC_IO_NAMING_CONVENTIONS_HXX
+#define CMC_NC_IO_NAMING_CONVENTIONS_HXX
+
+#include <string>
 
 namespace cmc
 {
-    
+
+enum CompressionScheme {UndefinedCompression = -1, NoCompression = 0, AdaptiveCoarsening = 0x01, PrefixExtraction = 0x10, AdaptiveCoarseningPlusPrefixExtraction = 0x11};
+inline const std::string kCompressionSchemeAttrName = "compression_scheme";
+typedef int CompressionSchemeType;
+
+
 }
 
-#endif /* !CMC_NC_IO_NAMING_CONVENTIONS */
+#endif /* !CMC_NC_IO_NAMING_CONVENTIONS_HXX */
