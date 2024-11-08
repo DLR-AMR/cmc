9188bd3dc195fe0b56afbcbc9b013c95475131c0
Niklas <niklas.boeing@gmx.de>
2024-11-08 08:15:28 +0100

    Clean up

--- src/lossy/cmc_amr_lossy_compression.hxx
+++ src/lossy/cmc_amr_lossy_compression.hxx
@@ -56,16 +56,14 @@ public:
     void Setup();
     void Setup(const AmrMesh& mesh);
     void Compress(const CompressionMode compression_mode = CompressionMode::OneForOne);
-    void SupplementarySZLikeCompression();
-    //void Compress(const char* path_to_file);
+
     void Decompress();
     OutputVar DecompressVariable(const int variable_id);
-    //void Decompress(const char* path_to_file);
+
     
     void SetMPICommunicator(const MPI_Comm communicator);
     void SetCompressionSettings(const CompressionSettings& settings);
     void SetCompressionSettings(CompressionSettings&& settings);
-    //void WriteVTKData();
 
     void WriteCompressedData(const std::string& file_name) const;
     void WriteVTKFile(const std::string& file_name) const;
@@ -74,42 +72,17 @@ public:
     size_t GetNumberOfCompressionVariables() const;
     size_t GetNumberOfInputVariables() const;
 
-    #if 0
-    Var GetVariable(const int id) const
-    {
-        cmc_assert(compression_data_ != nullptr);
-        return compression_data_->GetVariable(id);
-    };
-    Var&& GetVariable(const int id)
-    {
-        cmc_assert(compression_data_ != nullptr);
-        return std::move(compression_data_->GetVariable(id));
-    };
-    #endif
-
     OutputVar SeizeDecompressedVariable(const int variable_id);
     
 private:
-    //friend class TestLossyCompression;
-    
     void CompressionHasBeenApplied();
     void DecompressionHasBeenApplied();
 
-
-    //std::unique_ptr<AmrData> compression_data_;
     std::unique_ptr<AmrData> compression_data_{nullptr};
     CompressionSettings compression_settings_;
     CompressionMode compression_mode_{CompressionMode::CompressionModeUndefined};
     MPI_Comm comm_{MPI_COMM_WORLD};
     bool is_compression_applied_{false};
-
-    #if 0
-    MPI_Comm comm_{MPI_COMM_WORLD};
-    std::unique_ptr<AmrData> compression_data_;
-    std::shared_ptr<CompressionSettings> compression_settings_;
-    CompressionMode compression_mode_{CompressionMode::CompressionModeUndefined};
-    bool is_compression_applied_{false};
-    #endif
 };
 
 
