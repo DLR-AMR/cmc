81eec05be86b51961f81e3e3ce45c17a3f82bcd0
Niklas <niklas.boeing@gmx.de>
2025-03-05 14:32:23 +0100

    Split the error domain definition from the error domain functions

--- src/utilities/cmc_error_domain.hxx
+++ src/utilities/cmc_error_domain.hxx
@@ -2,6 +2,7 @@
 #define CMC_ERROR_DOMAIN_HXX
 
 #include "t8code/cmc_t8_mesh.hxx"
+#include "utilities/cmc_error_domain_functions.hxx"
 
 #include <vector>
 #include <utility>
@@ -10,10 +11,6 @@ namespace cmc
 {
 
 enum CompressionCriterion {CriterionUndefined, RelativeErrorThreshold, AbsoluteErrorThreshold};
-
-typedef bool IsElementInDomainFn(t8_forest_t forest, int tree_id, int lelement_id,
-                                 t8_eclass_scheme_c* ts, const t8_element_t * element);
-
 
 struct PermittedError
 {
@@ -82,18 +79,6 @@ private:
     const PermittedError error_criterion_;
     const IsElementInDomainFn* is_inside_domain_check_;
 };
-
-namespace error_domain_fn
-{
-
-inline bool
-GeneralErrorCriterion([[maybe_unused]] t8_forest_t forest, [[maybe_unused]] int tree_id, [[maybe_unused]] int first_lelement_id,
-                      [[maybe_unused]] t8_eclass_scheme_c* ts, [[maybe_unused]] const t8_element_t * element)
-{
-    return true;
-}
-
-}
 
 }
 
---
+++ src/utilities/cmc_error_domain_functions.hxx
@@ -0,0 +1,29 @@
+#ifndef CMC_ERROR_DOMAIN_FUNCTIONS_HXX
+#define CMC_ERROR_DOMAIN_FUNCTIONS_HXX
+
+#include "t8code/cmc_t8_mesh.hxx"
+
+namespace cmc
+{
+
+/* Typedef for a general error domain function determining
+ * whether a specific element is considered as "inside" the domain */
+typedef bool IsElementInDomainFn(t8_forest_t forest, int tree_id, int lelement_id,
+                                 t8_eclass_scheme_c* ts, const t8_element_t * element);
+
+namespace error_domain_fn
+{
+
+/* An error domain function resembling a global error criterion that holds for all elements */
+inline bool
+GeneralErrorCriterion([[maybe_unused]] t8_forest_t forest, [[maybe_unused]] int tree_id, [[maybe_unused]] int first_lelement_id,
+                      [[maybe_unused]] t8_eclass_scheme_c* ts, [[maybe_unused]] const t8_element_t * element)
+{
+    return true;
+}
+
+}
+
+}
+
+#endif /* !CMC_ERROR_DOMAIN_FUNCTIONS_HXX */
