ed9117ce8b612e5e9820077f96e2991ee91bd716
Niklas <niklas.boeing@gmx.de>
2025-03-10 08:49:44 +0100

    Changed the alphabet type

--- src/utilities/cmc_entropy_alphabet.hxx
+++ src/utilities/cmc_entropy_alphabet.hxx
@@ -1,9 +1,9 @@
 #ifndef CMC_ENTROPY_ALPHABET_HXX
 #define CMC_ENTROPY_ALPHABET_HXX
 
-#include <vector>
+#include <unordered_map>
 
-namespace cmc
+namespace cmc::entropy_coding
 {
 
 struct Letter
@@ -12,11 +12,11 @@ struct Letter
     uint32_t frequency;
 };
 
-typedef std::vector<Letter> Alphabet;
+typedef std::unordered_map<uint32_t, uint32_t> Alphabet;
 
 class IEntropyAlphabet
 {
-    virtual void InitializeSymbols() = 0;
+    virtual void InitializeSymbols(const size_t type_size_in_bytes) = 0;
     virtual void UpdateSymbol(const uint32_t symbol) = 0;
     virtual Alphabet GetSymbolFrequencies() = 0;
     virtual ~IEntropyAlphabet(){};
