6272359a7e165eb2277716f18bc4b717271a14ce
Niklas <niklas.boeing@gmx.de>
2025-03-10 09:35:55 +0100

    Corrected typos

--- src/utilities/cmc_entropy_alphabet.hxx
+++ src/utilities/cmc_entropy_alphabet.hxx
@@ -16,11 +16,12 @@ typedef std::unordered_map<uint32_t, uint32_t> Alphabet;
 
 class IEntropyAlphabet
 {
+public:
     virtual void InitializeSymbols(const size_t type_size_in_bytes) = 0;
     virtual void UpdateSymbol(const uint32_t symbol) = 0;
     virtual Alphabet GetSymbolFrequencies() = 0;
     virtual ~IEntropyAlphabet(){};
-}
+};
 
 }
 
