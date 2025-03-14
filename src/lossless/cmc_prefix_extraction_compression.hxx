02c208e3235fee478ced95a6872bf3dabd26f16d
Niklas <niklas.boeing@gmx.de>
2025-03-14 10:57:02 +0100

    Implemented the encoding of the level-wise prefix extraction data

--- src/lossless/cmc_prefix_extraction_compression.hxx
+++ src/lossless/cmc_prefix_extraction_compression.hxx
@@ -2,17 +2,23 @@
 #define CMC_PREFIX_EXTRACTION_COMPRESSION_HXX
 
 #include "utilities/cmc_bit_map.hxx"
+#include "utilities/cmc_bit_vector.hxx"
+#include "utilities/cmc_byte_value.hxx"
 #include "utilities/cmc_byte_compression_values.hxx"
 #include "lossless/cmc_byte_compression_variable.hxx"
-//#include "utilities/cmc_iface_lossless_compression_data.hxx"
+#include "utilities/cmc_arithmetic_encoding.hxx"
 
+#include <utility>
+#include <vector>
+#include <array>
+#include <algorithm>
 
 namespace cmc::lossless::prefix
 {
 
 /* A typedef for the sake of brevity */
 template <typename T>
-using CompressionValue = SerializedCompressionValue<sizeof(T)>;
+using CompressionValue = CompressionValue<T>;
 
 template<typename T>
 class PrefixAdaptData : public ICompressionAdaptData<T>
@@ -20,43 +26,28 @@ class PrefixAdaptData : public ICompressionAdaptData<T>
 public:
     PrefixAdaptData() = delete;
     PrefixAdaptData(AbstractByteCompressionVariable<T>* variable)
-    : ICompressionAdaptData<T>(variable) {};
+    : ICompressionAdaptData<T>(variable) {
+        ICompressionAdaptData<T>::entropy_coder_ = std::make_unique<cmc::entropy_coding::arithmetic_coding::Encoder>();
+    };
 
     void InitializeExtractionIteration() override;
     void FinalizeExtractionIteration() override;
     void CompleteExtractionIteration(const t8_forest_t previous_forest, const t8_forest_t adapted_forest) override;
     void RepartitionData(const t8_forest_t adapted_forest, const t8_forest_t partitioned_forest) override;
 
-    std::vector<uint8_t> EncodeLevelData(const std::vector<SerializedCompressionValue<sizeof(T)>>& level_byte_values) const override;
+    std::vector<uint8_t> EncodeLevelData(const std::vector<CompressionValue<T>>& level_byte_values) const override;
     
 protected:
     ExtractionData<T> PerformExtraction(const int which_tree, const int lelement_id, const int num_elements, const VectorView<CompressionValue<T>> values) override;
     UnchangedData<T> ElementStaysUnchanged(const int which_tree, const int lelement_id, const CompressionValue<T>& value) override;
 
 private:
-    void IndicateCommonPrefixFound();
-    void IndicateNoCommonPrefix();
     void IndicateCoarsening();
     void IndicateElementStaysUnchanged();
 
     bit_map::BitMap refinement_indications_;
-    bit_map::BitMap prefix_indications_;
     int count_adaptation_step_{0};
 };
-
-template<typename T>
-inline void
-PrefixAdaptData<T>::IndicateCommonPrefixFound()
-{
-    prefix_indications_.AppendSetBit();
-}
-
-template<typename T>
-inline void
-PrefixAdaptData<T>::IndicateNoCommonPrefix()
-{
-    prefix_indications_.AppendUnsetBit();
-}
 
 template<typename T>
 inline void
@@ -77,7 +68,6 @@ void
 PrefixAdaptData<T>::InitializeExtractionIteration()
 {
     refinement_indications_ = bit_map::BitMap();
-    prefix_indications_ = bit_map::BitMap();
 }
 
 template <typename T>
@@ -156,9 +146,6 @@ PrefixAdaptData<T>::PerformExtraction([[maybe_unused]] const int which_tree, [[m
 
     if (is_prefix_found)
     {
-        /* Indicate that we have found a common prefix */
-        IndicateCommonPrefixFound();
-
         std::vector<CompressionValue<T>> fine_values;
         fine_values.reserve(values.size());
 
@@ -173,9 +160,6 @@ PrefixAdaptData<T>::PerformExtraction([[maybe_unused]] const int which_tree, [[m
         return ExtractionData<T>(prefix, std::move(fine_values));
     } else
     {
-        /* Indicate that we have not found a common prefix */
-        IndicateNoCommonPrefix();
-
         std::vector<CompressionValue<T>> fine_values;
         fine_values.reserve(values.size());
 
@@ -197,20 +181,142 @@ PrefixAdaptData<T>::ElementStaysUnchanged(const int which_tree, const int leleme
     /* In case the element stays unchanged, we indicate that no coarsening is possible */
     IndicateElementStaysUnchanged();
 
-    /* Therefore, it is not possible to extract a common prefix as well */
-    IndicateNoCommonPrefix();
-
     /* We drag this value along to the "coarser level" until it this element is passed with its siblings
      *  as a family of elements into the adaptation callback. Moreover, we leave an empty value 
      * on the "finer level" */
     return UnchangedData<T>(value, CompressionValue<T>());
 }
 
+/**
+ * @brief Pushes a value byte-wise in Big-Endian ordering back to a vector
+ * 
+ * @tparam T The data type of the \a value
+ * @param byte_stream The stream to which the value is byte-wise appended
+ * @param value The value to be pushed back
+ */
+template <typename T>
+void
+PushBackValueToByteStream(std::vector<uint8_t>& byte_stream, const T& value)
+{
+    /* Serialize the value to a collection of bytes (in big endian order) */
+    const std::array<uint8_t, sizeof(T)> serialized_value = SerializeValue(value, Endian::Big);
+
+    /* Append the the bytes to the byte stream */
+    std::copy_n(serialized_value.begin(), sizeof(T), std::back_insert_iterator(byte_stream));
+}
+
+/**
+ * @brief  We use an arithmetic encoder to encode the position of the first "one-bit" in the compression value
+ * 
+ * @tparam T The original data type of the underlying data (e.g. float)
+ * @param level_byte_values The remaining "fine compression" values after an extraction iteration
+ * @return std::vector<uint8_t> The encoded data stream
+ */
 template <typename T>
 std::vector<uint8_t>
-PrefixAdaptData<T>::EncodeLevelData(const std::vector<SerializedCompressionValue<sizeof(T)>>& level_byte_values) const
+PrefixAdaptData<T>::EncodeLevelData(const std::vector<CompressionValue<T>>& level_byte_values) const
 {
-    return std::vector<uint8_t>();
+    //TODO: Rework for parallel usage
+
+    cmc_debug_msg("The encoding of the CompressionValues after the prefix extraction iteration starts...");
+    
+    cmc_assert(ICompressionAdaptData<T>::entropy_coder_ != nullptr);
+
+    /* The encoded data will be stored in a BitVector */
+    cmc::bit_vector::BitVector encoding;
+    encoding.Reserve(3 * level_byte_values.size());
+
+    /* If we are on the last level*/
+    /* Reset the entropy coder and initialize the alphabet */
+    ICompressionAdaptData<T>::entropy_coder_->Reset();
+    ICompressionAdaptData<T>::entropy_coder_->InitializeAlphabet(sizeof(T));
+
+    /* Collect the symbol frequencies */
+    for (auto val_iter = level_byte_values.begin(); val_iter != level_byte_values.end(); ++val_iter)
+    {
+        /* Get the leading zero count in the suffix (which is equivalent to the position of the fist "one-bit") */
+        const uint32_t lzc = static_cast<uint32_t>(val_iter->GetLeadingZeroCountInSignificantBits());
+
+        /* Update the frequency */
+        ICompressionAdaptData<T>::entropy_coder_->UpdateSymbolFrequency(lzc);
+    }
+
+    /* Setup the interior structure for encoding */
+    ICompressionAdaptData<T>::entropy_coder_->SetupEncoding();
+
+    /* Iterate over all values and encode them */
+    for (auto val_iter = level_byte_values.begin(); val_iter != level_byte_values.end(); ++val_iter)
+    {
+        /* Get the current value */
+        CompressionValue<T> current_val = *val_iter;
+
+        /* Get the leading zero count in the suffix (which is equivalent to the position of the fist "one-bit") */
+        const int lzc = current_val.GetLeadingZeroCountInSignificantBits();
+
+        /* Encode the leading zero count which equals the first "one-bit" position */
+        ICompressionAdaptData<T>::entropy_coder_->EncodeSymbol(static_cast<uint32_t>(lzc));
+
+        /* The remaining bits in the value are encoded/stored */
+        if (lzc == 0 && not current_val.IsEmpty())
+        {
+            /* In case there is no LZC the whol value need to be stored */
+            encoding.AppendBits(current_val.GetSignificantBitsInBigEndianOrdering(), current_val.GetCountOfSignificantBits());
+        } else
+        {
+            /* When its greater than zero, we can trim the zeros */
+            current_val.SetFrontBit(current_val.GetFrontBit() + lzc);
+
+            if (not current_val.IsEmpty())
+            {
+                encoding.AppendBits(current_val.GetSignificantBitsInBigEndianOrdering(), current_val.GetCountOfSignificantBits());
+            }
+        }
+
+    }
+
+    /* Indicate that the encoding has been finished and flush all pending encodings */
+    ICompressionAdaptData<T>::entropy_coder_->FinishEncoding();
+
+    /* Get the encoded alphabet */
+    cmc::bit_vector::BitVector encoded_alphabet = ICompressionAdaptData<T>::entropy_coder_->EncodeAlphabet();
+
+    /* Get the encoded LZC, respectivel first "one-bit" positions */
+    cmc::bit_map::BitMap encoded_lzc_stream = ICompressionAdaptData<T>::entropy_coder_->GetEncodedBitStream();
+
+    /* Collect the overall bytes for the encoding */
+    const size_t overall_level_bytes = 5 * sizeof(size_t) + refinement_indications_.size_bytes() + encoded_alphabet.size() + encoded_lzc_stream.size_bytes() + encoding.size();
+
+    /* Now everything is put together to a single stream */
+    std::vector<uint8_t> encoded_stream;
+    encoded_stream.reserve(overall_level_bytes);
+    
+    /* Push back the overall byte count for the level */
+    PushBackValueToByteStream(encoded_stream, overall_level_bytes);
+
+    /* Push back the refinement indications */
+    const size_t refinement_indications_bytes = refinement_indications_.size_bytes();
+    PushBackValueToByteStream(encoded_stream, refinement_indications_bytes);
+
+    /* Push back the byte count for the encoded alphabet */
+    const size_t encoded_alphabet_bytes = encoded_alphabet.size();
+    PushBackValueToByteStream(encoded_stream, encoded_alphabet_bytes);
+
+    /* Push back the byte count for the encoded first "one-bit" positions */
+    const size_t encoded_lzc_stream_bytes = encoded_lzc_stream.size_bytes();
+    PushBackValueToByteStream(encoded_stream, encoded_lzc_stream_bytes);
+
+    /* Push back the byte count for the remaining bits of the prefixes */
+    const size_t reamaining_value_bytes = encoding.size();
+    PushBackValueToByteStream(encoded_stream, reamaining_value_bytes);
+
+    /* Push back the refinement_indications, the encoded alphabet, the encoded LZC and the remaining bits in the given order */
+    std::copy_n(refinement_indications_.begin_bytes(), refinement_indications_bytes, std::back_insert_iterator(encoded_stream));
+    std::copy_n(encoded_alphabet.begin(), encoded_alphabet_bytes, std::back_insert_iterator(encoded_stream));
+    std::copy_n(encoded_lzc_stream.begin_bytes(), encoded_lzc_stream_bytes, std::back_insert_iterator(encoded_stream));
+    std::copy_n(encoding.begin(), reamaining_value_bytes, std::back_insert_iterator(encoded_stream));
+
+    cmc_debug_msg("The entropy encoder of the prefix ectraction compression stored the CompressionValues of this iteration within ", overall_level_bytes, " bytes.");
+    return encoded_stream;
 }
 
 template <typename T>
@@ -246,7 +352,7 @@ public:
         AbstractByteCompressionVariable<T>::adaptation_destructor_ = DestroyPrefixExtractionAdaptationClass<T>;
     };
 
-    CompressionVariable(const std::string& name, t8_forest_t initial_mesh, const std::vector<SerializedCompressionValue<sizeof(T)>>& variable_data)
+    CompressionVariable(const std::string& name, t8_forest_t initial_mesh, const std::vector<CompressionValue<T>>& variable_data)
     : AbstractByteCompressionVariable<T>()
     {
         if (static_cast<size_t>(t8_forest_get_local_num_elements(initial_mesh)) != variable_data.size())
@@ -261,7 +367,7 @@ public:
         AbstractByteCompressionVariable<T>::adaptation_destructor_ = DestroyPrefixExtractionAdaptationClass<T>;
     };
 
-    CompressionVariable(const std::string& name, t8_forest_t initial_mesh, std::vector<SerializedCompressionValue<sizeof(T)>>&& variable_data)
+    CompressionVariable(const std::string& name, t8_forest_t initial_mesh, std::vector<CompressionValue<T>>&& variable_data)
     : AbstractByteCompressionVariable<T>()
     {
         if (static_cast<size_t>(t8_forest_get_local_num_elements(initial_mesh)) != variable_data.size())
