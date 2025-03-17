3bd786f2f345b28aa0a78a5a775a55216991d1e9
Niklas <niklas.boeing@gmx.de>
2025-03-17 19:05:31 +0100

    Started a small test on the lossless multi-resolution compression variable

---
+++ test/cmc_multi_res_compression_variable.cxx
@@ -0,0 +1,45 @@
+#include "cmc.hxx"
+#include "test/cmc_test.hxx"
+#include "t8code/cmc_t8_mesh.hxx"
+#include "lossless/cmc_multi_res_extraction_compression.hxx"
+
+
+#include <numeric>
+
+int
+main(void)
+{
+    /* Initialize cmc */
+    cmc::CmcInitialize();
+    {
+
+    /* Create a mesh */
+    const sc_MPI_Comm comm = sc_MPI_COMM_SELF;
+    t8_cmesh_t cmesh;
+    cmesh = t8_cmesh_new_hypercube (T8_ECLASS_QUAD, comm, 0, 0, 0);
+    t8_scheme_cxx_t* scheme = t8_scheme_new_default_cxx ();
+    const int initial_level = 3;
+    t8_forest_t forest = t8_forest_new_uniform (cmesh, scheme, initial_level, 0, comm);
+    t8_eclass_scheme_c* ts = t8_forest_get_eclass_scheme(forest, t8_forest_get_eclass(forest, 0));
+
+    /* Create a variable for containing a value for each element */
+    const t8_locidx_t num_elements = t8_forest_get_local_num_elements(forest);
+    std::vector<float> data(num_elements);
+    std::iota(data.begin(), data.end(), 0.0);
+
+    cmc::lossless::multi_res::CompressionVariable<float> var("test_var", forest, data);
+
+    var.Compress();
+
+    std::vector<std::vector<uint8_t>> encoding;
+    var.MoveEncodedDataInto(encoding);
+
+    cmc::cmc_debug_msg("Levelwise encoding: levels: ", encoding.size());
+    
+    
+    }
+    /* Finalize cmc */
+    cmc::CmcFinalize();
+
+    return cmc::CMC_TEST_SUCCESS;
+}
