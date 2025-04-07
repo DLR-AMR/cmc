d6c9bdcbc1a3199b30939940dded9f39f4868638
Niklas <niklas.boeing@gmx.de>
2025-04-07 12:05:53 +0200

    Outsourced a minimal interface for an entropy coding alphabet

--- src/utilities/cmc_iface_entropy_alphabet.hxx
+++ src/utilities/cmc_iface_entropy_alphabet.hxx
@@ -1,5 +1,5 @@
-#ifndef CMC_ENTROPY_ALPHABET_HXX
-#define CMC_ENTROPY_ALPHABET_HXX
+#ifndef CMC_IFACE_ENTROPY_ALPHABET_HXX
+#define CMC_IFACE_ENTROPY_ALPHABET_HXX
 
 #include <unordered_map>
 
@@ -19,11 +19,10 @@ class IEntropyAlphabet
 public:
     virtual void InitializeSymbols(const size_t type_size_in_bytes) = 0;
     virtual void UpdateSymbol(const uint32_t symbol) = 0;
-    virtual Alphabet GetSymbolFrequencies() = 0;
     virtual ~IEntropyAlphabet(){};
 };
 
 }
 
 
-#endif /*! CMC_ENTROPY_ALPHABET_HXX */
+#endif /*! CMC_IFACE_ENTROPY_ALPHABET_HXX */
