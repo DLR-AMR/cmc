42c2da07a11a5d080fcfe2450562d0f8aed8d601
Niklas <niklas.boeing@gmx.de>
2025-04-16 20:25:51 +0200

    Added some convenience functions for appending and viewing several bits at once

--- src/utilities/cmc_bit_map.hxx
+++ src/utilities/cmc_bit_map.hxx
@@ -3,6 +3,7 @@
 
 
 #include "utilities/cmc_utilities.hxx"
+#include "utilities/cmc_log_functions.hxx"
 
 #include <vector>
 #include <climits>
@@ -61,6 +62,25 @@ public:
     BitMap(const size_t num_bits)
     : vector_(num_bits / kCharBit + (num_bits % kCharBit != 0 ? 1 : 0), 0), num_bits_{num_bits}{};
 
+    BitMap(const std::vector<uint8_t>& bitmap_conform_bitfield, const size_t num_bits)
+    : vector_(bitmap_conform_bitfield), num_bits_{num_bits}{
+        if (bitmap_conform_bitfield.size() != (num_bits / kCharBit + (num_bits % kCharBit != 0 ? 1 : 0)) || num_bits == 0)
+        {
+            throw std::invalid_argument("The supplied vector size does not match the amount of bits or the amount of bits is zero.");
+        }
+        byte_position_ = vector_.size() - 1;
+        bit_position_ = (num_bits % kCharBit == 0 ? kCharBit : (num_bits % kCharBit));
+    };
+    BitMap(std::vector<uint8_t>&& bitmap_conform_bitfield, const size_t num_bits)
+    : vector_(std::move(bitmap_conform_bitfield)), num_bits_{num_bits}{
+        if (bitmap_conform_bitfield.size() != (num_bits / kCharBit + (num_bits % kCharBit != 0 ? 1 : 0)) || num_bits == 0)
+        {
+            throw std::invalid_argument("The supplied vector size does not match the amount of bits or the amount of bits is zero.");
+        }
+        byte_position_ = vector_.size() - 1;
+        bit_position_ = (num_bits % kCharBit == 0 ? kCharBit : (num_bits % kCharBit));
+    };
+
     BitMap(const BitMap& other) = default;
     BitMap& operator=(const BitMap& other) = default;
     BitMap(BitMap&& other) = default;
@@ -98,6 +118,13 @@ public:
 
     size_t size() const {return num_bits_;};
     size_t size_bytes() const {return vector_.size();};
+    bool IsEmpty() const {return num_bits_ == 0;}; 
+
+    const uint8_t* data() const {return vector_.data();};
+
+    uint8_t* data_overwrite() {return &vector_[0];}
+
+    void AppendBits(const BitMap& bitmap);
 
     const std::vector<uint8_t>& GetByteData() const {return vector_;};
     void MoveDataInto(std::vector<uint8_t>& vector);
@@ -124,6 +151,8 @@ public:
     void MoveToStartBit(const size_t global_bit_position);
 
     bool GetNextBit();
+    
+    std::vector<uint8_t> GetNextNumberOfBits(const size_t num_bits);
 
     void MoveToNextByte();
 
@@ -153,6 +182,67 @@ BitMapView::GetNextBit()
         return next_bit;
     }
 }
+
+inline
+uint8_t
+GetNullifyBitMaskMSB(const int num_nonzero_bits)
+{
+    cmc_assert(num_nonzero_bits >= 0 && num_nonzero_bits <= kCharBitAsInt);
+    return (uint8_t{0xFF} >> (kCharBit - num_nonzero_bits));
+}
+
+inline std::vector<uint8_t>
+BitMapView::GetNextNumberOfBits(const size_t num_bits)
+{
+    if (num_bits == 0) {return std::vector<uint8_t>();}
+        
+    /* Potentially, we need to set the pointer to the next bit correctly */
+    if (bit_position_ == kCharBit) {++byte_position_; bit_position_ = 0;}
+
+    /* Number of bytes and bits needed to be extracted */
+    const size_t num_bytes_ = num_bits / kCharBit;
+    const size_t num_bits_ = num_bits % kCharBit;
+
+    /* Save the start byte position */
+    const int starting_byte_position = byte_position_;
+
+    /* Number of bytes to allocate */
+    const size_t num_bytes = num_bytes_ + (num_bits_ != 0 ? 1 : 0);
+    std::vector<uint8_t> bit_sequence(num_bytes, 0);
+
+    /* Define the shift operations for the extraction */
+    const int bit_shift_to_front = kCharBitAsInt - static_cast<int>(bit_position_);
+    const int bit_shift_to_back = bit_position_;
+
+    /* Get the bits in the correct order and store them in the output bit sequence */
+    for (size_t num_bits_extracted = 0, byte_id = 0; num_bits_extracted < num_bits; ++byte_id, ++byte_position_)
+    {
+        if (bit_shift_to_back > 0 && num_bits_extracted != 0)
+        {
+            bit_sequence[byte_id - 1] |= (data_[byte_position_] << bit_shift_to_front);
+            num_bits_extracted += (kCharBitAsInt - bit_shift_to_front);
+
+            if (num_bits_extracted >= num_bits){break;}
+        }
+
+        bit_sequence[byte_id] |= (data_[byte_position_] >> bit_shift_to_back);
+
+        num_bits_extracted += (kCharBitAsInt - bit_shift_to_back);
+    }
+
+    /* Update the byte and bit position of the view correctly */
+    byte_position_ = starting_byte_position + num_bytes_ + (num_bits_ + bit_position_ < kCharBit ? 0 : 1);
+    bit_position_ = (bit_position_ + num_bits_) % kCharBit;
+
+    /* Potentially, we need to erase the upper bits from the last extracted byte */
+    if (num_bits_ != 0)
+    {
+        bit_sequence.back() &= GetNullifyBitMaskMSB(num_bits_);
+    }
+
+    return bit_sequence;
+}
+
 inline void
 BitMapView::MoveToNextByte()
 {
@@ -231,6 +321,73 @@ BitMap::AppendUnsetBit()
     }
     ++num_bits_;
 }
+
+/**
+ * @brief Append the bits from another bitmap 
+ * 
+ * @param bitmap The bitmap whose bits will be appended to this bitmap 
+ */
+inline void
+BitMap::AppendBits(const BitMap& bitmap)
+{
+    if (&bitmap == this)
+    {
+        throw std::invalid_argument("The BitMap cannot append it's content to itself.");
+    }
+
+    if (bitmap.size() == 0){return;}
+
+    /* Potentially, prepare the setup to start the copying process */
+    if (bit_position_ >= kCharBit)
+    {
+        vector_.emplace_back(0);
+        ++byte_position_;
+        bit_position_ = 0;
+    } else if (bit_position_ == 0 && num_bits_ == 0)
+    {
+        /* In this case, we remove the preset byte in the vector, because it will be added later */
+        vector_.pop_back();
+    }
+
+    /* Define the shift operations */
+    const int shift_to_back = kCharBit - bit_position_;
+    const int shift_to_front = bit_position_;
+
+    /* Append all bytes from the bitmap to the vector */
+    for (auto byte_iter = bitmap.begin_bytes(); byte_iter != bitmap.end_bytes(); ++byte_iter)
+    {
+        /* Get the current byte */
+        uint8_t byte = *byte_iter;
+
+        if (shift_to_front == 0)
+        {
+            vector_.emplace_back(0);
+        }
+
+        vector_.back() |= (byte << shift_to_front);
+
+        if (shift_to_front > 0)
+        {
+            vector_.emplace_back(0);
+            vector_.back() |= (byte >> shift_to_back);
+        }
+    }
+
+    /* Potentially, we need to remove the last byte, since it may be empty */
+    if (shift_to_front > 0 && (((bitmap.size() % kCharBit) + bit_position_ <=  kCharBit)))
+    {
+        vector_.pop_back();
+    }
+
+    /* Set the byte and bit indicators correctly */
+    byte_position_ = vector_.size() - 1;
+    bit_position_ = ((bitmap.size() % kCharBit) + bit_position_  == kCharBit ? kCharBit : (bit_position_ + bitmap.size()) % kCharBit);
+
+    /* Update the bit count */
+    num_bits_ += bitmap.size();
+}
+
+
 
 /**
  * @brief Toggle a bit at the given position
