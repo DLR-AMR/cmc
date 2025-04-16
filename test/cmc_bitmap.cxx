abdd268d63001ad87db4bda3c5a5c77b6419c86f
Niklas <niklas.boeing@gmx.de>
2025-04-16 20:32:25 +0200

    Extended the test to the new BitMap functionality of appending several bits

--- test/cmc_bitmap.cxx
+++ test/cmc_bitmap.cxx
@@ -1,7 +1,6 @@
 #include "cmc.hxx"
 #include "test/cmc_test.hxx"
 #include "utilities/cmc_bit_map.hxx"
-#include "utilities/cmc_log_functions.hxx"
 
 int
 main(void)
@@ -68,6 +67,68 @@ main(void)
         }
         ++idx;
     }
+
+    /* Fill a BitMap */
+    cmc::bit_map::BitMap bitmap2;
+    bitmap2.AppendBit(true); //0b00000001
+    bitmap2.AppendBit(false); //0b00000001
+    bitmap2.AppendBit(true); //0b00000101
+    bitmap2.AppendBit(true); //0b00001101
+
+    /* Copy constructor */
+    cmc::bit_map::BitMap bitmap3 = bitmap2; //0b00001101
+
+    /* Append a whole BitMap */
+    bitmap3.AppendBits(bitmap2); //0b11011101
+
+    cmc::ExpectTrue(bitmap3.size() == 8);
+
+    /* Check whether the bitmap has been appended correctly */
+    const uint8_t encoded_byte = *(bitmap3.data());
+    cmc::ExpectTrue(encoded_byte == 221);
+
+    /* Append some more */
+    bitmap3.AppendBit(true);  //0b11011101 00000001
+    bitmap3.AppendBit(false); //0b11011101 00000001
+    bitmap3.AppendBit(false); //0b11011101 00000001
+    bitmap3.AppendBit(false); //0b11011101 00000001
+    bitmap3.AppendBit(false); //0b11011101 00000001
+    bitmap3.AppendBit(true);  //0b11011101 00100001
+
+
+    /* Append one to bitmap2 */
+    bitmap2.AppendBit(true); //0b00011101
+
+    /* And again append bitap2 to bitmap3 */
+    bitmap3.AppendBits(bitmap2); //0b11011101 01100001 00000111
+
+    cmc::ExpectTrue(bitmap3.size() == 19);
+
+    const uint8_t first_encoded_byte = *(bitmap3.data());
+    const uint8_t second_encoded_byte = *(bitmap3.data() + 1);
+    const uint8_t third_encoded_byte = *(bitmap3.data() + 2);
+
+    cmc::ExpectTrue(first_encoded_byte == 221);
+    cmc::ExpectTrue(second_encoded_byte == 97);
+    cmc::ExpectTrue(third_encoded_byte == 7);
+
+    /* Append the BitMap itself */
+    cmc::bit_map::BitMap bitmap4 = bitmap3; //0b00001101
+    bitmap3.AppendBits(bitmap4); //0b11011101 01100001 11101111 00001110 00111011
+
+    cmc::ExpectTrue(bitmap3.size() == 38);
+
+    const uint8_t again_first_encoded_byte = *(bitmap3.data());
+    const uint8_t again_second_encoded_byte = *(bitmap3.data() + 1);
+    const uint8_t again_third_encoded_byte = *(bitmap3.data() + 2);
+    const uint8_t again_fourth_encoded_byte = *(bitmap3.data() + 3);
+    const uint8_t again_fifth_encoded_byte = *(bitmap3.data() + 4);
+
+    cmc::ExpectTrue(again_first_encoded_byte == 221);
+    cmc::ExpectTrue(again_second_encoded_byte == 97);
+    cmc::ExpectTrue(again_third_encoded_byte == 239);
+    cmc::ExpectTrue(again_fourth_encoded_byte == 14);
+    cmc::ExpectTrue(again_fifth_encoded_byte == 59);
 
     /* Finalize cmc */
     cmc::CmcFinalize();
