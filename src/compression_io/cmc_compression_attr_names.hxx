611c2ac4786077657dc0e371f109727afb636383
Niklas <niklas.boeing@gmx.de>
2025-05-02 11:14:53 +0200

    Added some attributes and their respective names

--- src/compression_io/cmc_compression_attr_names.hxx
+++ src/compression_io/cmc_compression_attr_names.hxx
@@ -12,6 +12,11 @@ namespace cmc::compression_io
     const std::string mesh_id_attr("mesh_id");
     const std::string compression_schema_attr("compression_schema");
     const std::string num_compression_iterations_attr("num_compression_iterations");
+    const std::string are_refinement_bits_stored_attr("boolean_are_refinement_bits_stored");
+    const std::string missing_value_attr("missing_value");
+    const std::string initial_data_layout_attr("init_data_layout");
+    const std::string pre_compression_layout_attr("pre_compression_layout");
+    const std::string global_context_information_attr("info");
 
     inline std::string GenerateMeshVariableName(const int id)
     {
