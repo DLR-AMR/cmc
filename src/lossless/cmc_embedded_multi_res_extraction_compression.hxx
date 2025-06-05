b2748377fc816af80c059dc6e00d1030fe3a1b28
Niklas <niklas.boeing@gmx.de>
2025-06-05 12:39:55 +0200

    Removed the separate handling of the leaf level

--- src/lossless/cmc_embedded_multi_res_extraction_compression.hxx
+++ src/lossless/cmc_embedded_multi_res_extraction_compression.hxx
@@ -43,14 +43,13 @@ public:
 
     std::pair<std::vector<uint8_t>, std::vector<uint8_t>> EncodeLevelData(const std::vector<CompressionValue<T>>& level_byte_values) const override;
     std::pair<std::vector<uint8_t>, std::vector<uint8_t>> EncodeRootLevelData(const std::vector<CompressionValue<T>>& root_level_values) const override;
-    std::pair<std::vector<uint8_t>, std::vector<uint8_t>> EncodeLeafLevelData(const std::vector<CompressionValue<T>>& level_byte_values) const;
-protected:
+
+    protected:
     ExtractionData<T> PerformExtraction(const int which_tree, const int lelement_id, const int num_elements, const VectorView<CompressionValue<T>> values) override;
     UnchangedData<T> ElementStaysUnchanged(const int which_tree, const int lelement_id, const CompressionValue<T>& value) override;
 
 private:
     void CollectSymbolFrequenciesForEntropyCoding(const std::vector<CompressionValue<T>>& level_byte_values) const;
-    void CollectLeafLevelSymbolFrequenciesForEntropyCoding(const std::vector<CompressionValue<T>>& level_byte_values) const;
     
     bit_map::BitMap resdiual_order_indications_;
     int count_adaptation_step_{0};
@@ -204,7 +203,6 @@ MultiResEmbeddedAdaptData<T>::CollectSymbolFrequenciesForEntropyCoding(const std
     }
 }
 
-static bool leaf_elem_encoding = false;
 /**
  * @brief  We use an arithmetic encoder to encode the position of the first "one-bit" in the compression value
  * 
@@ -216,12 +214,6 @@ template <typename T>
 std::pair<std::vector<uint8_t>, std::vector<uint8_t>>
 MultiResEmbeddedAdaptData<T>::EncodeLevelData(const std::vector<CompressionValue<T>>& level_byte_values) const
 {
-    if (leaf_elem_encoding == true)
-    {
-        leaf_elem_encoding = false;
-        return EncodeLeafLevelData(level_byte_values);
-    }
-    
     cmc_debug_msg("The encoding of the CompressionValues after the multi-resolution extraction iteration starts...");
     
     cmc_assert(IEmbeddedCompressionAdaptData<T>::entropy_coder_ != nullptr);
@@ -351,151 +343,6 @@ MultiResEmbeddedAdaptData<T>::EncodeLevelData(const std::vector<CompressionValue
     return std::make_pair(encoded_entropy_codes, encoded_data);
 }
 
-
-template <typename T>
-void
-MultiResEmbeddedAdaptData<T>::CollectLeafLevelSymbolFrequenciesForEntropyCoding(const std::vector<CompressionValue<T>>& level_byte_values) const
-{
-    /* Collect the symbol frequencies */
-    for (auto val_iter = level_byte_values.begin(); val_iter != level_byte_values.end(); ++val_iter)
-    {
-        /* Get the length of this prefix */
-        const uint32_t tail_bit_pos = static_cast<uint32_t>(val_iter->GetTailBit());
-
-        /* Update the frequency */
-        IEmbeddedCompressionAdaptData<T>::entropy_coder_->UpdateSymbolFrequency(tail_bit_pos);
-    }
-}
-
-/**
- * @brief  We use an arithmetic encoder to encode the position of the first "one-bit" in the compression value
- * 
- * @tparam T The original data type of the underlying data (e.g. float)
- * @param level_byte_values The remaining "fine compression" values after an extraction iteration
- * @return std::vector<uint8_t> The encoded data stream
- */
-template <typename T>
-std::pair<std::vector<uint8_t>, std::vector<uint8_t>>
-MultiResEmbeddedAdaptData<T>::EncodeLeafLevelData(const std::vector<CompressionValue<T>>& level_byte_values) const
-{
-    cmc_debug_msg("The encoding of the leaf level CompressionValues after the prefix extraction iteration starts...");
-    
-    cmc_assert(IEmbeddedCompressionAdaptData<T>::entropy_coder_ != nullptr);
-
-    /* Get the rank of the mpi process within the communicator */
-    int rank{0};
-    int ret_val = MPI_Comm_rank(this->GetMPIComm(), &rank);
-    MPICheckError(ret_val);
-
-    /* Define the root rank */
-    const int root_rank = 0;
-
-    /* The encoded data will be stored in a BitVector */
-    cmc::bit_vector::BitVector encoding;
-    encoding.Reserve(3 * level_byte_values.size());
-
-    /* Reset the entropy coder and initialize the alphabet */
-    IEmbeddedCompressionAdaptData<T>::entropy_coder_->Reset(std::make_unique<cmc::entropy_coding::arithmetic_coding::MultiResCompressionAlphabet<T>>());
-    IEmbeddedCompressionAdaptData<T>::entropy_coder_->InitializeAlphabet(sizeof(T));
-
-    /* Collect the symbols and their frequencies for the entropy coder */
-    CollectLeafLevelSymbolFrequenciesForEntropyCoding(level_byte_values);
-
-    /* Setup the interior structure for encoding */
-    IEmbeddedCompressionAdaptData<T>::entropy_coder_->SetupEncoding(this->GetMPIComm());
-
-    /* Iterate over all values and encode them */
-    for (auto val_iter = level_byte_values.begin(); val_iter != level_byte_values.end(); ++val_iter)
-    {
-        /* Get the current value */
-        CompressionValue<T> current_val = *val_iter;
-
-        /* Get the length of the prefix */
-        const int pref_length = current_val.GetCountOfSignificantBits();
-        const int tail_bit_pos = current_val.GetTailBit();
-        /* Encode the prefix length */
-        IEmbeddedCompressionAdaptData<T>::entropy_coder_->EncodeSymbol(static_cast<uint32_t>(tail_bit_pos));
-
-        /* In case there is a prefix, the actual bits of the prefix will be stored */
-        if (pref_length > 0)
-        {
-            encoding.AppendBits(current_val.GetSignificantBitsInBigEndianOrdering(), pref_length);
-        }
-    }
-
-    /* We set an indication symbol that the process local end of the values have been reached */
-    IEmbeddedCompressionAdaptData<T>::entropy_coder_->EncodeSymbol(entropy_coding::arithmetic_coding::kByteCompressionSymbolJumpToNextByte);
-
-    /* Indicate that the encoding has been finished and flush all pending encodings */
-    IEmbeddedCompressionAdaptData<T>::entropy_coder_->FinishEncoding();
-
-    /* Set up the BitVector holding the encoded data for further use */
-    encoding.TrimToContent();
-
-    /* Get the local remaining significant bits */
-    const uint64_t local_remaining_significant_bits_num_bytes = static_cast<uint64_t>(encoding.size());
-
-    /* Get the local encoded prefix lengths */
-    cmc::bit_map::BitMap local_encoded_prefix_length_stream = IEmbeddedCompressionAdaptData<T>::entropy_coder_->GetEncodedBitStream();
-    const uint64_t local_encoded_prefix_length_stream_num_bytes = static_cast<uint64_t>(local_encoded_prefix_length_stream.size_bytes());
-
-    /* We need exchange the encoded lengths */
-    const std::vector<uint64_t> local_bytes{local_encoded_prefix_length_stream_num_bytes, local_remaining_significant_bits_num_bytes};
-    std::vector<uint64_t> global_bytes{0, 0};
-
-    ret_val = MPI_Reduce(local_bytes.data(), global_bytes.data(), 2, MPI_UINT64_T, MPI_SUM, root_rank, this->GetMPIComm());
-    MPICheckError(ret_val);
-
-    /* Declare the buffers for the encoded data */
-    std::vector<uint8_t> encoded_entropy_codes;
-    std::vector<uint8_t> encoded_data;
-
-    /* Only the root rank needs to encode the encoded sizes */
-    if (rank == root_rank)
-    {
-        /* Get the encoded alphabet */
-        cmc::bit_vector::BitVector encoded_alphabet = IEmbeddedCompressionAdaptData<T>::entropy_coder_->EncodeAlphabet();
-        const uint64_t encoded_alphabet_num_bytes = static_cast<uint64_t>(encoded_alphabet.size());
-
-        /* Calculate the overall amount of bytes on the root rank */
-        const uint64_t num_locally_encoded_entropy_codes_bytes = 4 * sizeof(uint64_t) + encoded_alphabet_num_bytes + local_encoded_prefix_length_stream_num_bytes;
-
-        /* Allocate memory for the encoded data */
-        encoded_entropy_codes.reserve(num_locally_encoded_entropy_codes_bytes);
-
-        /** We store global information about the encoded level **/
-        /* Push back the overall byte count for the level */
-        const uint64_t num_global_bytes_encoded_level = 4 * sizeof(uint64_t) + encoded_alphabet_num_bytes + global_bytes[0] + global_bytes[1];
-        PushBackValueToByteStream<uint64_t>(encoded_entropy_codes, num_global_bytes_encoded_level);
-
-        /* Push back the byte count for the encoded alphabet */
-        PushBackValueToByteStream<uint64_t>(encoded_entropy_codes, encoded_alphabet_num_bytes);
-
-        /* Push back the byte count for the encoded first "one-bit" positions */
-        PushBackValueToByteStream<uint64_t>(encoded_entropy_codes, global_bytes[0]);
-
-        /* Push back the byte count for the remaining significant bits */
-        PushBackValueToByteStream<uint64_t>(encoded_entropy_codes, global_bytes[1]);
-
-        /* Afterwards, we store the encoded alphabet */
-        std::copy_n(encoded_alphabet.begin(), encoded_alphabet_num_bytes, std::back_inserter(encoded_entropy_codes));
-    
-        /* Finally, copy the entropy codes */
-        std::copy_n(local_encoded_prefix_length_stream.begin_bytes(), local_encoded_prefix_length_stream_num_bytes, std::back_insert_iterator(encoded_entropy_codes));
-    } else
-    {
-        /* Otherwise, the rank only hold the entropy codes */
-        local_encoded_prefix_length_stream.MoveDataInto(encoded_entropy_codes);
-    }
-
-    /* Get the encoded remaining significant bits */
-    encoding.MoveDataInto(encoded_data);
-
-    cmc_debug_msg("The entropy encoder of the prefix extraction compression completed the encoding of the CompressionValues of this iteration.");
-    
-    return std::make_pair(encoded_entropy_codes, encoded_data);
-}
-
 template <typename T>
 inline std::pair<std::vector<uint8_t>, std::vector<uint8_t>>
 MultiResEmbeddedAdaptData<T>::EncodeRootLevelData(const std::vector<CompressionValue<T>>& root_level_values) const
@@ -542,8 +389,6 @@ public:
         //cmc_assert(input_variable.IsValid());//TODO: implement 
 
         this->SetName(input_variable.GetName());
-        //this->SetMPIComm(input_variable.GetMPIComm());
-        //cmc_debug_msg("Here is comm: ", input_variable.GetMPIComm());
         this->IndicateWhetherMeshRefinementBitsWillBeStored(true);
 
         AbstractEmbeddedByteCompressionVariable<T>::adaptation_creator_ = CreateMultiResEmbeddedExtractionAdaptationClass<T>;
