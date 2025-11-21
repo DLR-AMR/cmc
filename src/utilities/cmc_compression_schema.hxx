6e95ba7ed32f0ce2ad11a8fbe7bbc5b17064686a
Niklas <niklas.boeing@gmx.de>
2025-11-21 12:19:24 +0100

    Added a new compression scheme to the enumerator

--- src/utilities/cmc_compression_schema.hxx
+++ src/utilities/cmc_compression_schema.hxx
@@ -4,7 +4,26 @@
 namespace cmc
 {
 
-enum CompressionSchema {AdaptiveCoarsening, TrimmedMultiResResiduals, PrefixExtraction, PrefixExtractionPlainSuffixes, MultiResExtraction, EmbeddedPrefixExtraction, EmbeddedPrefixExtractionTrimmedSuffixes, EmbeddedMultiResExtractionLinReconstruction, LossyEmbeddedMultiResExtractionNearestNeighborReconstruction, EmbeddedPrefixExtractionPlainSuffixes, EmbeddedQuantizedPrefixExtraction, EmbeddedMultiResExtraction, EmbeddedTrimmedMultiResExtraction, EmbeddedMultiResExtractionTrimmedResiduals, _TestEmbeddedPCP4Extraction, _TestPCP4Extraction};
+enum CompressionSchema
+{
+    AdaptiveCoarsening,
+    TrimmedMultiResResiduals,
+    PrefixExtraction,
+    PrefixExtractionPlainSuffixes,
+    MultiResExtraction,
+    EmbeddedPrefixExtraction,
+    EmbeddedPrefixExtractionTrimmedSuffixes,
+    EmbeddedMultiResExtractionLinReconstruction,
+    LossyEmbeddedMultiResExtractionNearestNeighborReconstruction,
+    EmbeddedPrefixExtractionPlainSuffixes,
+    EmbeddedQuantizedPrefixExtraction,
+    EmbeddedMultiResExtraction,
+    EmbeddedTrimmedMultiResExtraction,
+    EmbeddedMultiResExtractionTrimmedResiduals,
+    _TestEmbeddedPCP4Extraction,
+    _TestPCP4Extraction,
+    PatchPrefixExtractionPlainSuffixes
+};
 
 }
 
